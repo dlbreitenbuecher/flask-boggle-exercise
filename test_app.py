096bfc00c9a6f49be96b6b773fc947e657920fc3
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-07-24 11:39:56 -0700

    added a requirements.txt

--- test_app.py
+++ test_app.py
@@ -5,6 +5,11 @@ from app import app, boards
 from app import SESS_BOARD_UUID_KEY
 
 
+# Make Flask errors be real errors, not HTML pages with error info
+app.config['TESTING'] = True
+# This is a bit of hack, but don't use Flask DebugToolbar
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
 class BoggleAppTestCase(TestCase):
     """Test flask app of Boggle."""
 
